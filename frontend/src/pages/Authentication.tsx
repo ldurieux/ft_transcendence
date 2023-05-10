f5286200fdc2f191eb9acbd6466d42e1e80a0b8e
JackieRis <nguyentan28@hotmail.fr>
2023-05-10 04:46:12 +0200

    editing navbar, routes are working well and 42auth is working fine

--- frontend/src/pages/Authentication.tsx
+++ frontend/src/pages/Authentication.tsx
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import "../components/Styles/LoginStyles.css";
+
 class Authentication extends Component {
+
   render() {
     return (
+
       <div className="AuthBody">
-        <form>
-          <label>
-            Nickname :
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
+        <a href='https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-daf495cf13fd1f090c114ac2c7c8c9c0c15d11dee8de959157b2c07821a76d0d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code'>
+            Login
+        </a>
       </div>
     );
   }
